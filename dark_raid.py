a9e5184c494350531258a5878e192e183785b8f6
MrEmpy <71796063+MrEmpy@users.noreply.github.com>
2021-01-16 17:59:22 +0000

    Add files via upload

--- dark_raid.py
+++ dark_raid.py
@@ -27,7 +27,7 @@ def help():
 
 {Fore.LIGHTRED_EX}        ╔═════════════════════════╗
         ║{Fore.LIGHTWHITE_EX} Tool Created by Mr Empy {Fore.LIGHTRED_EX}║
-        ║{Fore.LIGHTWHITE_EX} Version 1.0             {Fore.LIGHTRED_EX}║
+        ║{Fore.LIGHTWHITE_EX} Version 1.5             {Fore.LIGHTRED_EX}║
         ╚═════════════════════════╝
 
 Como Usar:
@@ -35,12 +35,15 @@ Como Usar:
     -h            Help da Tool
     --help        Help da Tool
 
+    --check       Checar Tokens
+
     --join        Entrar num Servidor
 
     --leave       Sair de um Servidor
 
     --attack      Raidar um Servidor
     ''')
+
 
 def attack():
     print(f'''{Fore.LIGHTRED_EX}
@@ -57,16 +60,15 @@ def attack():
         ╚═════════════════════════╝
     ''')
 
-
     #f = open('tokens.txt', 'r')
     f = open('tokens.txt').read().splitlines()
     #tk = f.readlines()
-    id = input(f'{Fore.LIGHTRED_EX}ID do Canal onde vai ser Raidado: {Fore.LIGHTWHITE_EX}')
+    id = input(
+        f'{Fore.LIGHTRED_EX}ID do Canal onde vai ser Raidado: {Fore.LIGHTWHITE_EX}')
     msg = input(f'{Fore.LIGHTRED_EX}Mensagem da Raid: {Fore.LIGHTWHITE_EX}')
 
     api = f'https://discord.com/api/v8/channels/{id}/messages'
     print('')
-
 
     #tokens = token.replace('\n', '')
 
@@ -74,12 +76,12 @@ def attack():
         tokens = random.choice(f)
 
         payload = {
-        'content': msg
-}
+            'content': msg
+        }
 
         header = {
-        'authorization': tokens
-}
+            'authorization': tokens
+        }
         r = requests.post(api, data=payload, headers=header)
 
         if r.status_code == 200:
@@ -109,15 +111,16 @@ def join_server():
     tk = f.readlines()
 
     api = 'https://discord.com/api/v8/invites/'
-    id_server = input(f'{Fore.LIGHTRED_EX}ID do convite do servidor (ex 2ZRubYhv): {Fore.LIGHTWHITE_EX}')
+    id_server = input(
+        f'{Fore.LIGHTRED_EX}ID do convite do servidor (ex 2ZRubYhv): {Fore.LIGHTWHITE_EX}')
     print('')
 
     for token in tk:
         tokens = token.replace('\n', '')
 
         header = {
-        'authorization': tokens
-}
+            'authorization': tokens
+        }
 
         r = requests.post(api + id_server, headers=header)
 
@@ -147,15 +150,16 @@ def leave_server():
     tk = f.readlines()
 
     api = 'https://discord.com/api/v8/users/@me/guilds/'
-    id_server = input(f'{Fore.LIGHTRED_EX}ID do servidor: {Fore.LIGHTWHITE_EX}')
+    id_server = input(
+        f'{Fore.LIGHTRED_EX}ID do servidor: {Fore.LIGHTWHITE_EX}')
     print('')
 
     for token in tk:
         tokens = token.replace('\n', '')
 
         header = {
-        'authorization': tokens
-}
+            'authorization': tokens
+        }
 
         r = requests.delete(api + id_server, headers=header)
 
@@ -163,6 +167,45 @@ def leave_server():
             print(f'{Fore.LIGHTGREEN_EX}[+] Um bot saiu do servidor')
         else:
             print(f'{Fore.LIGHTRED_EX}[-] Algo deu errado')
+
+
+def check_tokens():
+    print(f'''{Fore.LIGHTRED_EX}
+ _____           _   __ ______      _     _
+|  _  \         | | / / | ___ \    (_)   | |
+| | | |__ _ _ __| |/ /  | |_/ /__ _ _  __| |
+| | | / _` | '__|    \  |    // _` | |/ _` |
+| |/ / (_| | |  | |\  \ | |\ \ (_| | | (_| |
+|___/ \__,_|_|  \_| \_/ \_| \_\__,_|_|\__,_|
+
+{Fore.LIGHTRED_EX}        ╔═════════════════════════╗
+        ║{Fore.LIGHTWHITE_EX} Tool Created by Mr Empy {Fore.LIGHTRED_EX}║
+        ║{Fore.LIGHTWHITE_EX} Version 1.0             {Fore.LIGHTRED_EX}║
+        ╚═════════════════════════╝
+
+''')
+
+    f = open('tokens.txt', 'r')
+    tk = f.readlines()
+    valid_tokens = open('valid_tokens.txt', 'a')
+
+    api = 'https://discord.com/api/v8/users/@me'
+    print(f'{Fore.LIGHTBLUE_EX}[*] {Fore.LIGHTRED_EX}Checking Tokens...')
+    print('')
+
+    for token in tk:
+        tokens = token.replace('\n', '')
+
+        header = {
+            'authorization': tokens
+        }
+        r = requests.get(api, headers=header).text
+
+        if 'email' in r:
+            print(f'{Fore.LIGHTGREEN_EX}[+] Funcionando: {token}')
+            valid_tokens.write(token)
+        else:
+            print(f'{Fore.LIGHTRED_EX}[-] Nao Funciona: {token}')
 
 
 if __name__ == '__main__':
@@ -180,3 +223,5 @@ if __name__ == '__main__':
 
     if menu[0] == '--leave':
         leave()
+    if menu[0] == '--check':
+        check_tokens()
